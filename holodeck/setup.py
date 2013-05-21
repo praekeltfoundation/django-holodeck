6fc48159ad544534c3a3480863d8c0f4ba2f7123
Milton <milton@praekeltfoundation.org>
2013-05-21 16:27:16 +0200

    added setup.py

---
+++ holodeck/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-holodeck',
+    version='0.1.0',
+    description='Publishes GA stats to holodeck',
+    long_description=open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-holodeck',
+    packages=find_packages(exclude=['project', ]),
+    dependency_links=[
+    ],
+    install_requires=[
+        'django-celery',
+        'photon',
+        'django-google-credentials',
+    ],
+    tests_require=[],
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
